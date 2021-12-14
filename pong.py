2dfa8185dc8f2cf8c585750fd43fe913009f01cf
grorge <g97116@yahoo.com>
2021-12-14 20:51:42 +0800

    feat: pong game

---
+++ pong.py
@@ -0,0 +1,220 @@
+#PONG pygame
+
+import random
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+fps = pygame.time.Clock()
+
+#colors
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLACK = (0,0,0)
+#globals
+WIDTH = 1000
+HEIGHT = 600       
+BALL_RADIUS = 10
+PAD_WIDTH = 8
+PAD_HEIGHT = 80
+HALF_PAD_WIDTH = PAD_WIDTH // 2
+HALF_PAD_HEIGHT = PAD_HEIGHT // 2
+ball_num = 10
+colorlist = [RED] * ball_num
+ball_pos = [[0,0]] * ball_num
+ball_vel = [[0,0]] * ball_num
+paddle1_vel = [0, 0]
+paddle2_vel = [0, 0]
+paddle1_pos = [[0, 0], [0, 0]]
+paddle2_pos = [[0, 0], [0, 0]]
+l_score = 0
+r_score = 0
+
+#canvas declaration
+window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption('Hello World')
+
+# helper function that spawns a ball, returns a position vector and a velocity vector
+# if right is True, spawn to the right, else spawn to the left
+def ball_init(id):
+    global ball_pos, ball_vel # these are vectors stored as lists
+    ball_pos[id] = [WIDTH//2,HEIGHT//2]
+    horz = random.randrange(2,4)
+    vert = random.randrange(1,3)
+    colorlist[id] = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+    if random.randrange(0,2) == 0:
+        horz = - horz
+        
+    ball_vel[id] = [horz,-vert]
+
+# define event handlers
+def init():
+    global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score  # these are floats
+    global score1, score2  # these are ints
+    paddle1_pos[0] = [HALF_PAD_WIDTH - 1,HEIGHT//2]
+    paddle2_pos[0] = [WIDTH +1 - HALF_PAD_WIDTH,HEIGHT//2]
+    paddle1_pos[1] = [HALF_PAD_WIDTH - 1 + 160,HEIGHT//2]
+    paddle2_pos[1] = [WIDTH +1 - HALF_PAD_WIDTH - 160,HEIGHT//2]
+    l_score = 0
+    r_score = 0
+    for i in range(ball_num):
+        ball_init(i)
+
+
+#draw function of canvas
+def draw(canvas):
+    global paddle1_pos, paddle2_pos, ball_pos, ball_vel, l_score, r_score
+           
+    canvas.fill(BLACK)
+    pygame.draw.line(canvas, WHITE, [WIDTH // 2, 0],[WIDTH // 2, HEIGHT], 1)
+    pygame.draw.line(canvas, WHITE, [PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1)
+    pygame.draw.line(canvas, WHITE, [WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1)
+    pygame.draw.circle(canvas, WHITE, [WIDTH//2, HEIGHT//2], 70, 1)
+
+    # update paddle's vertical position, keep paddle on the screen
+    if paddle1_pos[0][1] > HALF_PAD_HEIGHT and paddle1_pos[0][1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos[0][1] += paddle1_vel[0]
+    elif paddle1_pos[0][1] <= HALF_PAD_HEIGHT and paddle1_vel[0] > 0:
+        paddle1_pos[0][1] += paddle1_vel[0]
+    elif paddle1_pos[0][1] >= HEIGHT - HALF_PAD_HEIGHT and paddle1_vel[0] < 0:
+        paddle1_pos[0][1] += paddle1_vel[0]
+
+    if paddle1_pos[1][1] > HALF_PAD_HEIGHT and paddle1_pos[1][1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle1_pos[1][1] += paddle1_vel[1]
+    elif paddle1_pos[1][1] <= HALF_PAD_HEIGHT and paddle1_vel[1] > 0:
+        paddle1_pos[1][1] += paddle1_vel[1]
+    elif paddle1_pos[1][1] >= HEIGHT - HALF_PAD_HEIGHT and paddle1_vel[1] < 0:
+        paddle1_pos[1][1] += paddle1_vel[1]
+    
+    if paddle2_pos[0][1] > HALF_PAD_HEIGHT and paddle2_pos[0][1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos[0][1] += paddle2_vel[0]
+    elif paddle2_pos[0][1] <= HALF_PAD_HEIGHT and paddle2_vel[0] > 0:
+        paddle2_pos[0][1] += paddle2_vel[0]
+    elif paddle2_pos[0][1] >= HEIGHT - HALF_PAD_HEIGHT and paddle2_vel[0] < 0:
+        paddle2_pos[0][1] += paddle2_vel[0]
+
+    if paddle2_pos[1][1] > HALF_PAD_HEIGHT and paddle2_pos[1][1] < HEIGHT - HALF_PAD_HEIGHT:
+        paddle2_pos[1][1] += paddle2_vel[1]
+    elif paddle2_pos[1][1] <= HALF_PAD_HEIGHT and paddle2_vel[1] > 0:
+        paddle2_pos[1][1] += paddle2_vel[1]
+    elif paddle2_pos[1][1] >= HEIGHT - HALF_PAD_HEIGHT and paddle2_vel[1] < 0:
+        paddle2_pos[1][1] += paddle2_vel[1]
+
+    #update ball
+    for i in range(ball_num):
+        ball_pos[i][0] += int(ball_vel[i][0])
+        ball_pos[i][1] += int(ball_vel[i][1])
+
+    #draw paddles and ball
+    # pygame.draw.circle(canvas, RED, ball_pos, 20, 0)
+    for i in range(ball_num):
+        pygame.draw.rect(canvas, colorlist[i], [ball_pos[i][0]-BALL_RADIUS, ball_pos[i][1]-BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2], 2)
+    pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[0][0] - HALF_PAD_WIDTH, paddle1_pos[0][1] - HALF_PAD_HEIGHT], [paddle1_pos[0][0] - HALF_PAD_WIDTH, paddle1_pos[0][1] + HALF_PAD_HEIGHT], [paddle1_pos[0][0] + HALF_PAD_WIDTH, paddle1_pos[0][1] + HALF_PAD_HEIGHT], [paddle1_pos[0][0] + HALF_PAD_WIDTH, paddle1_pos[0][1] - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[0][0] - HALF_PAD_WIDTH, paddle2_pos[0][1] - HALF_PAD_HEIGHT], [paddle2_pos[0][0] - HALF_PAD_WIDTH, paddle2_pos[0][1] + HALF_PAD_HEIGHT], [paddle2_pos[0][0] + HALF_PAD_WIDTH, paddle2_pos[0][1] + HALF_PAD_HEIGHT], [paddle2_pos[0][0] + HALF_PAD_WIDTH, paddle2_pos[0][1] - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.polygon(canvas, GREEN, [[paddle1_pos[1][0] - HALF_PAD_WIDTH, paddle1_pos[1][1] - HALF_PAD_HEIGHT], [paddle1_pos[1][0] - HALF_PAD_WIDTH, paddle1_pos[1][1] + HALF_PAD_HEIGHT], [paddle1_pos[1][0] + HALF_PAD_WIDTH, paddle1_pos[1][1] + HALF_PAD_HEIGHT], [paddle1_pos[1][0] + HALF_PAD_WIDTH, paddle1_pos[1][1] - HALF_PAD_HEIGHT]], 0)
+    pygame.draw.polygon(canvas, GREEN, [[paddle2_pos[1][0] - HALF_PAD_WIDTH, paddle2_pos[1][1] - HALF_PAD_HEIGHT], [paddle2_pos[1][0] - HALF_PAD_WIDTH, paddle2_pos[1][1] + HALF_PAD_HEIGHT], [paddle2_pos[1][0] + HALF_PAD_WIDTH, paddle2_pos[1][1] + HALF_PAD_HEIGHT], [paddle2_pos[1][0] + HALF_PAD_WIDTH, paddle2_pos[1][1] - HALF_PAD_HEIGHT]], 0)
+    #ball collision check on top and bottom walls
+    for i in range(ball_num):
+        if int(ball_pos[i][1]) <= BALL_RADIUS:
+            ball_vel[i][1] = - ball_vel[i][1]
+        if int(ball_pos[i][1]) >= HEIGHT + 1 - BALL_RADIUS:
+            ball_vel[i][1] = -ball_vel[i][1]
+    
+    #ball collison check on gutters or paddles
+    for i in range(ball_num):
+        for q in range(2):
+            if int(ball_pos[i][0]) + BALL_RADIUS + ball_vel[i][0] >= paddle1_pos[q][0] - PAD_WIDTH and int(ball_pos[i][0]) <= paddle1_pos[q][0] - PAD_WIDTH and\
+                 int(ball_pos[i][1]) in range(paddle1_pos[q][1] - HALF_PAD_HEIGHT - BALL_RADIUS, paddle1_pos[q][1] + HALF_PAD_HEIGHT,1):
+                ball_vel[i][0] = -abs(ball_vel[i][0])
+                ball_vel[i][0] *= 1.2
+                ball_vel[i][1] *= 1.2
+            elif int(ball_pos[i][0]) - BALL_RADIUS + ball_vel[i][0] <= paddle1_pos[q][0] + PAD_WIDTH and int(ball_pos[i][0]) >= paddle1_pos[q][0] + PAD_WIDTH and\
+                int(ball_pos[i][1]) in range(paddle1_pos[q][1] - HALF_PAD_HEIGHT - BALL_RADIUS, paddle1_pos[q][1] + HALF_PAD_HEIGHT,1):
+                ball_vel[i][0] = abs(ball_vel[i][0])
+                ball_vel[i][0] *= 1.2
+                ball_vel[i][1] *= 1.2
+            elif int(ball_pos[i][0]) <= BALL_RADIUS + PAD_WIDTH:
+                r_score += 1
+                ball_init(i)
+    
+    for i in range(ball_num):
+        for q in range(2):
+            if int(ball_pos[i][0]) + BALL_RADIUS + ball_vel[i][0] >= paddle2_pos[q][0] - PAD_WIDTH and ball_pos[i][0] <= paddle2_pos[q][0] - PAD_WIDTH and\
+                 int(ball_pos[i][1]) in range(paddle2_pos[q][1] - HALF_PAD_HEIGHT - BALL_RADIUS, paddle2_pos[q][1] + HALF_PAD_HEIGHT,1):
+                ball_vel[i][0] = -(ball_vel[i][0])
+                ball_vel[i][0] *= 1.2
+                ball_vel[i][1] *= 1.2
+            elif int(ball_pos[i][0]) - BALL_RADIUS + ball_vel[i][0] <= paddle2_pos[q][0] + PAD_WIDTH and ball_pos[i][0] >= paddle2_pos[q][0] + PAD_WIDTH and\
+                int(ball_pos[i][1]) in range(paddle2_pos[q][1] - HALF_PAD_HEIGHT - BALL_RADIUS, paddle2_pos[q][1] + HALF_PAD_HEIGHT,1):
+                ball_vel[i][0] = abs(ball_vel[i][0])
+                ball_vel[i][0] *= 1.2
+                ball_vel[i][1] *= 1.2
+            elif int(ball_pos[i][0]) >= WIDTH + 1 - BALL_RADIUS - PAD_WIDTH:
+                l_score += 1
+                ball_init(i)
+
+    #update scores
+    myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
+    label1 = myfont1.render("Score "+str(l_score), 1, (255,255,0))
+    canvas.blit(label1, (50,20))
+
+    myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
+    label2 = myfont2.render("Score "+str(r_score), 1, (255,255,0))
+    canvas.blit(label2, (470, 20))  
+    
+    
+#keydown handler
+def keydown(event):
+    global paddle1_vel, paddle2_vel
+    
+    if event.key == K_UP:
+        paddle2_vel[0] = -8
+    elif event.key == K_DOWN:
+        paddle2_vel[0] = 8
+    elif event.key == K_w:
+        paddle1_vel[0] = -8
+    elif event.key == K_s:
+        paddle1_vel[0] = 8
+    elif event.key == K_u:
+        paddle2_vel[1] = -8
+    elif event.key == K_j:
+        paddle2_vel[1] = 8
+    elif event.key == K_y:
+        paddle1_vel[1] = -8
+    elif event.key == K_h:
+        paddle1_vel[1] = 8
+
+#keyup handler
+def keyup(event):
+    global paddle1_vel, paddle2_vel
+    
+    if event.key in (K_w, K_s):
+        paddle1_vel[0] = 0
+    elif event.key in (K_UP, K_DOWN):
+        paddle2_vel[0] = 0
+    elif event.key in (K_u, K_j):
+        paddle2_vel[1] = 0
+    elif event.key in (K_y, K_h):
+        paddle1_vel[1] = 0
+
+init()
+
+
+#game loop
+while True:
+
+    draw(window)
+
+    for event in pygame.event.get():
+
+        if event.type == KEYDOWN:
+            keydown(event)
+        elif event.type == KEYUP:
+            keyup(event)
+        elif event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+            
+    pygame.display.update()
+    fps.tick(60)
---
+++ pongGym.py
@@ -0,0 +1,210 @@
+import numpy as np
+from gym import spaces
+import random
+import pygame, sys
+from pygame.locals import *
+import operator
+import functools
+
+
+class DoublePong:
+	WHITE = (255,255,255)
+	RED = (255,0,0)
+	GREEN = (0,255,0)
+	BLACK = (0,0,0)
+	PAD_WIDTH = 8
+	PAD_HEIGHT = 80
+	BALL_RADIUS = 10
+	HALF_PAD_WIDTH = PAD_WIDTH // 2
+	HALF_PAD_HEIGHT = PAD_HEIGHT // 2
+	ball_num = 10
+	colorlist = [RED] * 50
+	def __init__(self, WIDTH = 1000, HEIGHT = 600, ball_num = 10):
+		self.WIDTH = WIDTH
+		self.HEIGHT = HEIGHT
+		self.ball_num = ball_num
+		self.action_space = spaces.Discrete(5)
+		observation_high = np.array([np.float32(max(WIDTH, HEIGHT))] * (ball_num * 2 + 2))
+		observation_low = np.array([np.float32(0)] * (ball_num * 2 + 2))
+		self.observation_space = spaces.Box(observation_low, observation_high)
+		self.ball_pos = [[0,0]] * ball_num
+		self.ball_vel = [[0,0]] * ball_num
+		self.paddle1_vel = [0, 0]
+		self.paddle2_vel = [0, 0]
+		self.paddle1_pos = [[0, 0], [0, 0]]
+		self.paddle2_pos = [[0, 0], [0, 0]]
+		self.colorlist = [(255,0,0)] * 50
+		self.l_score = 0
+		self.r_score = 0
+		self.first_show = True
+		self.counter = 0
+		self.reset()
+		pass
+	def reset(self):
+		# global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel,l_score,r_score  # these are floats
+		self.paddle1_pos[0] = [DoublePong.HALF_PAD_WIDTH - 1, self.HEIGHT//2]
+		self.paddle2_pos[0] = [self.WIDTH +1 - DoublePong.HALF_PAD_WIDTH, self.HEIGHT//2]
+		self.paddle1_pos[1] = [DoublePong.HALF_PAD_WIDTH - 1 + 160, self.HEIGHT//2]
+		self.paddle2_pos[1] = [self.WIDTH +1 - DoublePong.HALF_PAD_WIDTH - 160, self.HEIGHT//2]
+		self.l_score = 0
+		self.r_score = 0
+		for i in range(self.ball_num):
+			self.ball_init(i)
+		observation = []
+		for i in range(self.ball_num):
+			observation.append(self.ball_pos[i][0])
+			observation.append(self.ball_pos[i][1])
+		observation.append(self.paddle1_pos[0][1])
+		observation.append(self.paddle1_pos[1][1])
+		return observation
+	def ball_init(self, id):
+		self.ball_pos[id] = [self.WIDTH//2,self.HEIGHT//2]
+		horz = random.randrange(2,4)
+		vert = random.randrange(-3,3)
+		if vert == 0:
+			vert = vert + 1
+		self.colorlist[id] = (random.randrange(0,255),random.randrange(0,255),random.randrange(0,255))
+		if random.randrange(0,2) == 0:
+			horz = - horz
+			
+		self.ball_vel[id] = [horz,-vert]
+	def step(self, action):
+		observation = []
+		reward = 0
+		done = False if self.counter < 10800 else True
+		info = []
+		self.counter += 1
+		if action[0] == 1:
+			self.paddle1_vel[0] = 8
+		elif action[0] == 2:
+			self.paddle1_vel[0] = -8
+		elif action[0] == 3:
+			self.paddle1_vel[1] = 8
+		elif action[0] == 4:
+			self.paddle1_vel[1] = -8
+		
+		if self.paddle1_pos[0][1] > DoublePong.HALF_PAD_HEIGHT and self.paddle1_pos[0][1] < self.HEIGHT - DoublePong.HALF_PAD_HEIGHT:
+			self.paddle1_pos[0][1] += self.paddle1_vel[0]
+		elif self.paddle1_pos[0][1] <= DoublePong.HALF_PAD_HEIGHT and self.paddle1_vel[0] > 0:
+			self.paddle1_pos[0][1] += self.paddle1_vel[0]
+		elif self.paddle1_pos[0][1] >= self.HEIGHT - DoublePong.HALF_PAD_HEIGHT and self.paddle1_vel[0] < 0:
+			self.paddle1_pos[0][1] += self.paddle1_vel[0]
+
+		if self.paddle1_pos[1][1] > DoublePong.HALF_PAD_HEIGHT and self.paddle1_pos[1][1] < self.HEIGHT - DoublePong.HALF_PAD_HEIGHT:
+			self.paddle1_pos[1][1] += self.paddle1_vel[1]
+		elif self.paddle1_pos[1][1] <= DoublePong.HALF_PAD_HEIGHT and self.paddle1_vel[1] > 0:
+			self.paddle1_pos[1][1] += self.paddle1_vel[1]
+		elif self.paddle1_pos[1][1] >= self.HEIGHT - DoublePong.HALF_PAD_HEIGHT and self.paddle1_vel[1] < 0:
+			self.paddle1_pos[1][1] += self.paddle1_vel[1]
+		
+		if self.paddle2_pos[0][1] > DoublePong.HALF_PAD_HEIGHT and self.paddle2_pos[0][1] < self.HEIGHT - DoublePong.HALF_PAD_HEIGHT:
+			self.paddle2_pos[0][1] += self.paddle2_vel[0]
+		elif self.paddle2_pos[0][1] <= DoublePong.HALF_PAD_HEIGHT and self.paddle2_vel[0] > 0:
+			self.paddle2_pos[0][1] += self.paddle2_vel[0]
+		elif self.paddle2_pos[0][1] >= self.HEIGHT - DoublePong.HALF_PAD_HEIGHT and self.paddle2_vel[0] < 0:
+			self.paddle2_pos[0][1] += self.paddle2_vel[0]
+
+		if self.paddle2_pos[1][1] > DoublePong.HALF_PAD_HEIGHT and self.paddle2_pos[1][1] < self.HEIGHT - DoublePong.HALF_PAD_HEIGHT:
+			self.paddle2_pos[1][1] += self.paddle2_vel[1]
+		elif self.paddle2_pos[1][1] <= DoublePong.HALF_PAD_HEIGHT and self.paddle2_vel[1] > 0:
+			self.paddle2_pos[1][1] += self.paddle2_vel[1]
+		elif self.paddle2_pos[1][1] >= self.HEIGHT - DoublePong.HALF_PAD_HEIGHT and self.paddle2_vel[1] < 0:
+			self.paddle2_pos[1][1] += self.paddle2_vel[1]
+
+		self.paddle1_vel = [0, 0]
+
+		#update ball
+		for i in range(self.ball_num):
+			self.ball_pos[i][0] += int(self.ball_vel[i][0])
+			self.ball_pos[i][1] += int(self.ball_vel[i][1])
+
+		#ball collision check on top and bottom walls
+		for i in range(self.ball_num):
+			if int(self.ball_pos[i][1]) <= DoublePong.BALL_RADIUS:
+				self.ball_vel[i][1] = - self.ball_vel[i][1]
+			if int(self.ball_pos[i][1]) >= self.HEIGHT + 1 - DoublePong.BALL_RADIUS:
+				self.ball_vel[i][1] = -self.ball_vel[i][1]
+		
+		#ball collison check on gutters or paddles
+		for i in range(self.ball_num):
+			for q in range(2):
+				if int(self.ball_pos[i][0]) + DoublePong.BALL_RADIUS + self.ball_vel[i][0] >= self.paddle1_pos[q][0] - DoublePong.PAD_WIDTH and int(self.ball_pos[i][0]) <= self.paddle1_pos[q][0] - DoublePong.PAD_WIDTH and\
+					int(self.ball_pos[i][1]) >= self.paddle1_pos[q][1] - DoublePong.HALF_PAD_HEIGHT - DoublePong.BALL_RADIUS and int(self.ball_pos[i][1]) <= self.paddle1_pos[q][1] + DoublePong.HALF_PAD_HEIGHT:
+					self.ball_vel[i][0] = -abs(self.ball_vel[i][0])
+					self.ball_vel[i][0] *= 1.2
+					self.ball_vel[i][1] *= 1.2
+					reward -= abs(self.ball_vel[i][0])
+				elif int(self.ball_pos[i][0]) - DoublePong.BALL_RADIUS + self.ball_vel[i][0] <= self.paddle1_pos[q][0] + DoublePong.PAD_WIDTH and int(self.ball_pos[i][0]) >= self.paddle1_pos[q][0] + DoublePong.PAD_WIDTH and\
+					int(self.ball_pos[i][1]) >= self.paddle1_pos[q][1] - DoublePong.HALF_PAD_HEIGHT - DoublePong.BALL_RADIUS and int(self.ball_pos[i][1]) <= self.paddle1_pos[q][1] + DoublePong.HALF_PAD_HEIGHT:
+					self.ball_vel[i][0] = abs(self.ball_vel[i][0])
+					self.ball_vel[i][0] *= 1.2
+					self.ball_vel[i][1] *= 1.2
+					reward += abs(self.ball_vel[i][0])
+				elif int(self.ball_pos[i][0]) <= DoublePong.BALL_RADIUS + DoublePong.PAD_WIDTH:
+					self.r_score += 1
+					reward -= 10
+					self.ball_init(i)
+		
+		for i in range(self.ball_num):
+			for q in range(2):
+				if int(self.ball_pos[i][0]) + DoublePong.BALL_RADIUS + self.ball_vel[i][0] >= self.paddle2_pos[q][0] - DoublePong.PAD_WIDTH and self.ball_pos[i][0] <= self.paddle2_pos[q][0] - DoublePong.PAD_WIDTH and\
+					int(self.ball_pos[i][1]) in range(self.paddle2_pos[q][1] - DoublePong.HALF_PAD_HEIGHT - DoublePong.BALL_RADIUS, self.paddle2_pos[q][1] + DoublePong.HALF_PAD_HEIGHT,1):
+					self.ball_vel[i][0] = -(self.ball_vel[i][0])
+					self.ball_vel[i][0] *= 1.2
+					self.ball_vel[i][1] *= 1.2
+				elif int(self.ball_pos[i][0]) - DoublePong.BALL_RADIUS + self.ball_vel[i][0] <= self.paddle2_pos[q][0] + DoublePong.PAD_WIDTH and self.ball_pos[i][0] >= self.paddle2_pos[q][0] + DoublePong.PAD_WIDTH and\
+					int(self.ball_pos[i][1]) in range(self.paddle2_pos[q][1] - DoublePong.HALF_PAD_HEIGHT - DoublePong.BALL_RADIUS, self.paddle2_pos[q][1] + DoublePong.HALF_PAD_HEIGHT,1):
+					self.ball_vel[i][0] = abs(self.ball_vel[i][0])
+					self.ball_vel[i][0] *= 1.2
+					self.ball_vel[i][1] *= 1.2
+				elif int(self.ball_pos[i][0]) >= self.WIDTH + 1 - DoublePong.BALL_RADIUS - DoublePong.PAD_WIDTH:
+					self.l_score += 1
+					self.ball_init(i)
+		observation = []
+		for i in range(self.ball_num):
+			observation.append(self.ball_pos[i][0])
+			observation.append(self.ball_pos[i][1])
+		observation.append(self.paddle1_pos[0][1])
+		observation.append(self.paddle1_pos[1][1])
+		info = [self.l_score, self.r_score]
+		return [observation, reward, done, info]
+	def render(self):
+		if self.first_show:
+			pygame.init()
+			self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT), 0, 32)
+			pygame.display.set_caption('Hello World')
+			self.first_show = False
+		pass
+		self.window.fill(DoublePong.BLACK)
+		pygame.draw.line(self.window, self.WHITE, [self.WIDTH // 2, 0],[self.WIDTH // 2, self.HEIGHT], 1)
+		pygame.draw.line(self.window, self.WHITE, [DoublePong.PAD_WIDTH, 0],[DoublePong.PAD_WIDTH, self.HEIGHT], 1)
+		pygame.draw.line(self.window, self.WHITE, [self.WIDTH - DoublePong.PAD_WIDTH, 0],[self.WIDTH - DoublePong.PAD_WIDTH, self.HEIGHT], 1)
+		pygame.draw.circle(self.window, self.WHITE, [self.WIDTH//2, self.HEIGHT//2], 70, 1)
+		for i in range(self.ball_num):
+			pygame.draw.rect(self.window, self.colorlist[i], [self.ball_pos[i][0]-DoublePong.BALL_RADIUS, self.ball_pos[i][1]-DoublePong.BALL_RADIUS, DoublePong.BALL_RADIUS * 2, DoublePong.BALL_RADIUS * 2], 2)
+		pygame.draw.polygon(self.window, DoublePong.GREEN, [[self.paddle1_pos[0][0] - DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[0][1] - DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[0][0] - DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[0][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[0][0] + DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[0][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[0][0] + DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[0][1] - DoublePong.HALF_PAD_HEIGHT]], 0)
+		pygame.draw.polygon(self.window, DoublePong.GREEN, [[self.paddle2_pos[0][0] - DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[0][1] - DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[0][0] - DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[0][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[0][0] + DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[0][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[0][0] + DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[0][1] - DoublePong.HALF_PAD_HEIGHT]], 0)
+		pygame.draw.polygon(self.window, DoublePong.GREEN, [[self.paddle1_pos[1][0] - DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[1][1] - DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[1][0] - DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[1][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[1][0] + DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[1][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle1_pos[1][0] + DoublePong.HALF_PAD_WIDTH, self.paddle1_pos[1][1] - DoublePong.HALF_PAD_HEIGHT]], 0)
+		pygame.draw.polygon(self.window, DoublePong.GREEN, [[self.paddle2_pos[1][0] - DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[1][1] - DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[1][0] - DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[1][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[1][0] + DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[1][1] + DoublePong.HALF_PAD_HEIGHT], [self.paddle2_pos[1][0] + DoublePong.HALF_PAD_WIDTH, self.paddle2_pos[1][1] - DoublePong.HALF_PAD_HEIGHT]], 0)
+		#update scores
+		myfont1 = pygame.font.SysFont("Comic Sans MS", 20)
+		label1 = myfont1.render("Score "+str(self.l_score), 1, (255,255,0))
+		self.window.blit(label1, (50,20))
+
+		myfont2 = pygame.font.SysFont("Comic Sans MS", 20)
+		label2 = myfont2.render("Score "+str(self.r_score), 1, (255,255,0))
+		self.window.blit(label2, (470, 20)) 
+		pygame.display.update()
+	def close(self):
+		if not self.first_show:
+			pygame.quit()
+		pass
+env = DoublePong()
+fps = pygame.time.Clock()
+st = env.reset()
+done = False
+while not done:
+	action = [random.randrange(0,6)]
+	observation, reward, done, info = env.step(action)
+	env.render()
+# 	fps.tick(60)
